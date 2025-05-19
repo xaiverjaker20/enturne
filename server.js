fa09349a14f5825a22c2394499743c24ba2ae49d
xaiverjaker20 <xaiverjaker20@hotmail.com>
2025-05-18 22:51:52 -0500

    Actualizo base datos

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use(express.json());
 
 // Configuración de la conexión a MySQL
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'turnos_descarga'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 // Conectar a la base de datos
