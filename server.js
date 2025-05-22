b6fd934cce643035f9ea43c97f8195550f05ee08
xaiverjaker20 <xaiverjaker20@hotmail.com>
2025-05-21 23:35:22 -0500

    actualizar datos de railway server.js

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.use(express.json());
 
 // Configuración de la conexión a MySQL
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: 'shortline.proxy.rlwy.net', 
+    user: 'root',
+    password: 'cUlGjxXYaJmNTynxeURdxXjDcJmGRpOF',
+    database: 'railway',
+    port: 14150
+
 });
 
 // Conectar a la base de datos
